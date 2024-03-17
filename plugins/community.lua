400788cb8f4ff7dc2dd4b9bb194572db9144b84a
Martin Robinson <martin.robinson@atu.ie>
2024-03-17 20:03:51 +0000

    Copied over tabularise, vimtex and zen-mode plugins into the user/plugin directory which worked with previous configuration.

--- plugins/community.lua
+++ plugins/community.lua
@@ -5,6 +5,22 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   { import = "astrocommunity.colorscheme.catppuccin" },
-  { import = "astrocommunity.markdown-and-latex.vimtex"},
+  { -- further customize the options set by the community
+    "catppuccin",
+    opts = {
+      integrations = {
+        sandwich = false,
+        noice = true,
+        mini = true,
+        leap = true,
+        markdown = true,
+        neotest = true,
+        cmp = true,
+        overseer = true,
+        lsp_trouble = true,
+        rainbow_delimiters = true,
+      },
+    },
+  },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
